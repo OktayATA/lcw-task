b47d6ca678ac9615ae5c0c175f57ba206e73522c
oktayata <oktayata@gmail.com>
2022-03-11 18:18:35 +0300

    refactoring

--- assets/script/script.js
+++ assets/script/script.js
@@ -17,3 +17,62 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 4000);
 }
+
+function btnFirst(){
+
+  var first = document.getElementById("btn-first");
+  var second = document.getElementById("btn-second");
+
+  if(first.style.display === "none"){
+
+    second.style.display ="none"
+    first.style.display = "block";
+    document.getElementById("btn1").style.backgroundColor = "blue";
+    document.getElementById("btn2").style.backgroundColor = "grey";
+
+    
+  }
+  else{
+    second.style.display ="block"
+    first.style.display = "none";
+    document.getElementById("btn1").style.backgroundColor = "grey";
+    document.getElementById("btn2").style.backgroundColor = "blue";
+  }
+
+  console.log("mamama")
+
+}
+function btnSecond(){
+
+
+  var first = document.getElementById("btn-first");
+  var second = document.getElementById("btn-second");
+
+   
+  if(second.style.display === "none"){
+
+    
+    first.style.display ="none"
+    second.style.display = "block";
+    document.getElementById("btn1").style.backgroundColor = "grey";
+    document.getElementById("btn2").style.backgroundColor = "blue";
+    
+  }
+  else if(second.style.display === "block"){
+    first.style.display ="block"
+    second.style.display = "none";
+    document.getElementById("btn1").style.backgroundColor = "blue";
+    document.getElementById("btn2").style.backgroundColor = "grey";
+
+  }
+  else{
+    first.style.display ="none"
+    second.style.display = "block";
+    document.getElementById("btn1").style.backgroundColor = "grey";
+    document.getElementById("btn2").style.backgroundColor = "blue";
+
+  }
+
+  console.log("ajajaja")
+  
+}
